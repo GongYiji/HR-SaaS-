7c27e2b10296dcf6dcf4afc9059fe4f0ce4205e5
gongyijie666 <10340347+gongyijie666@user.noreply.gitee.com>
2022-09-22 16:24:59 +0800

    读取公司角色信息

---
+++ src/api/setting.js
@@ -0,0 +1,21 @@
+import request from '@/utils/request'
+
+/**
+ * 获取角色列表
+ */
+export function getRoleList(params) {
+  return request({
+    url: '/sys/role',
+    params
+  })
+}
+
+/**
+ * 根据id查询企业
+ */
+export function getCompanyInfo(id) {
+  return request({
+    url: `/company/${id}`
+
+  })
+}
--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -14,11 +14,11 @@
               >新增角色</el-button>
             </el-row>
             <!-- 表格 -->
-            <el-table border="">
-              <el-table-column label="序号" width="120" />
-              <el-table-column label="角色名称" width="240" />
-              <el-table-column label="描述" />
-              <el-table-column label="操作">
+            <el-table border="" :data="list">
+              <el-table-column align="center" type="index" label="序号" width="120" />
+              <el-table-column align="center" prop="name" label="角色名称" width="240" />
+              <el-table-column align="center" prop="description" label="描述" />
+              <el-table-column align="center" label="操作">
                 <el-button size="small" type="success">分配权限</el-button>
                 <el-button size="small" type="primary">编辑</el-button>
                 <el-button size="small" type="danger">删除</el-button>
@@ -27,9 +27,16 @@
             <!-- 分页组件 -->
             <el-row type="flex" justify="center" align="middle" style="height: 60px">
               <!-- 分页组件 -->
-              <el-pagination layout="prev,pager,next" />
+              <el-pagination
+                :current-page.sync="page.page"
+                :page-size="page.pagesize"
+                :total="page.total"
+                layout="prev,pager,next"
+                @current-change="changePage"
+              />
             </el-row>
           </el-tab-pane>
+
           <el-tab-pane label="公司信息">
             <el-alert
               title="对公司名称、公司地址、营业执照、公司地区的更新，将使得公司资料被重新审核，请谨慎修改"
@@ -39,16 +46,16 @@
             />
             <el-form label-width="120px" style="margin-top:50px">
               <el-form-item label="公司名称">
-                <el-input disabled style="width:400px" />
+                <el-input v-model="formData.name" disabled style="width:400px" />
               </el-form-item>
               <el-form-item label="公司地址">
-                <el-input disabled style="width:400px" />
+                <el-input v-model="formData.companyAddress" disabled style="width:400px" />
               </el-form-item>
               <el-form-item label="邮箱">
-                <el-input disabled style="width:400px" />
+                <el-input v-model="formData.mailbox" disabled style="width:400px" />
               </el-form-item>
               <el-form-item label="备注">
-                <el-input type="textarea" :rows="3" disabled style="width:400px" />
+                <el-input v-model="formData.remarks" type="textarea" :rows="3" disabled style="width:400px" />
               </el-form-item>
             </el-form>
           </el-tab-pane>
@@ -60,8 +67,59 @@
 </template>
 
 <script>
-export default {
+import { getRoleList, getCompanyInfo } from '@/api/setting'
+import { mapGetters } from 'vuex'
+// import store from '@/store'
 
+export default {
+  name: 'Setting',
+  data() {
+    return {
+      list: [], // 承接数组
+      page: {
+        // 放置页码及相关数据
+        page: 1,
+        pagesize: 2,
+        total: 0 // 记录总数
+      },
+      formData: {
+        name: '',
+        companyAddress: '',
+        mailbox: '',
+        remarks: ''
+
+      }
+      // companyId: {}
+    }
+  },
+
+  computed: {
+    ...mapGetters(['companyId'])
+
+  },
+  created() {
+    this.getRoleList() // 获取角色列表
+    this.getCompanyInfo()
+  },
+  methods: {
+    async getRoleList() {
+      const res = await getRoleList(this.page)
+      console.log(res)
+      this.page.total = res.total
+      this.list = res.rows
+    },
+    changePage(newPage) {
+      // newPage是当前点击的页码
+      this.page.page = newPage// 将当前页码赋值给当前的最新页码
+      this.getRoleList()
+    },
+    // 获取的公司的信息
+    async getCompanyInfo() {
+      // console.log(store.getters.companyId)
+      this.formData = await getCompanyInfo(this.companyId)
+      console.log(this.formData)
+    }
+  }
 }
 </script>
 
